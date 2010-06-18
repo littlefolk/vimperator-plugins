27d298c590be64a529a8744ca5a0341d0f2966b3
littlefolk <littlefolk@gmail.com>
2010-06-18 18:22:43 +0900

    Changed, move_loaded_tab.js. Minimize code.

--- move_loaded_tab.js
+++ move_loaded_tab.js
@@ -1,16 +1,18 @@
 // 次の未読タブへ移動 {{{
 (function(){
   if (liberator.plugins.browser_object_api) {
-    let tap = function () (browser_object_api.istap(gBrowser.mCurrentTab)? 0: 1);
+    let bo = liberator.plugins.browser_object_api;
+    let ts = TreeStyleTabService;
+    let tap = function () (bo.istap(gBrowser.mCurrentTab)? 0: 1);
     [
       [
         ['gj', 'sj'], 'Go to the next tab *loaded*.',
         function (count) {
           tabs.select(
             (
-              browser_object_api.loaded(browser_object_api.right())[(count || tap())] ||
-              browser_object_api.loaded()[((count && count - 1) || 0)] ||
-              browser_object_api.loaded().pop()
+              bo.loaded(bo.right())[(count || tap())] ||
+              bo.loaded()[((count && count - 1) || 0)] ||
+              bo.loaded().pop()
             )._tPos
           );
         }
@@ -19,33 +21,33 @@
         function (count) {
           tabs.select(
             (
-              browser_object_api.loaded(browser_object_api.left()).reverse()[(count || tap())] ||
-              browser_object_api.loaded().reverse()[((count && count - 1) || 0)] ||
-              browser_object_api.loaded().shift()
+              bo.loaded(bo.left()).reverse()[(count || tap())] ||
+              bo.loaded().reverse()[((count && count - 1) || 0)] ||
+              bo.loaded().shift()
             )._tPos
           );
         }
       ], [
         ['gJ', 'sJ'], 'Go to the next *root* tab *loaded*.',
         function (count) {
-          let parent = TreeStyleTabService.getParentTab(gBrowser.mCurrentTab);
+          let parent = ts.getParentTab(gBrowser.mCurrentTab);
           tabs.select(
             (
-              browser_object_api.right(browser_object_api.loaded(TreeStyleTabService.rootTabs))[((parent)? 0: (count || tap()))] ||
-              browser_object_api.loaded(TreeStyleTabService.rootTabs)[((count && count - 1) || 0)] ||
-              browser_object_api.loaded(TreeStyleTabService.rootTabs).pop()
+              bo.right(bo.loaded(ts.rootTabs))[((parent)? 0: (count || tap()))] ||
+              bo.loaded(ts.rootTabs)[((count && count - 1) || 0)] ||
+              bo.loaded(ts.rootTabs).pop()
             )._tPos
           );
         }
       ], [
         ['gK', 'sK'], 'Go to the previous *root* tab *loaded*.',
         function (count) {
-          let parent = TreeStyleTabService.getParentTab(gBrowser.mCurrentTab);
+          let parent = ts.getParentTab(gBrowser.mCurrentTab);
           tabs.select(
             (
-              browser_object_api.left(browser_object_api.loaded(TreeStyleTabService.rootTabs)).reverse()[(parent)? 0: (count || tap())] ||
-              browser_object_api.loaded(TreeStyleTabService.rootTabs).reverse()[((count && count - 1) || 0)] ||
-              browser_object_api.loaded().shift()
+              bo.left(bo.loaded(ts.rootTabs)).reverse()[(parent)? 0: (count || tap())] ||
+              bo.loaded(ts.rootTabs).reverse()[((count && count - 1) || 0)] ||
+              bo.loaded().shift()
             )._tPos
           );
         }
