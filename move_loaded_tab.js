bc189516de996bade4c5bd644001c71d3c9695a2
littlefolk <littlefolk@gmail.com>
2010-06-22 22:38:03 +0900

    Changed, move_loaded_tab.js. Commonize.

--- move_loaded_tab.js
+++ move_loaded_tab.js
@@ -4,59 +4,32 @@
     let bo = liberator.plugins.browser_object_api;
     let ts = TreeStyleTabService;
     let tap = function () (bo.istap(gBrowser.mCurrentTab)? 0: 1);
-    let carryover = function (all, remnant, count) {
-      let i = ((count && (count % all.length) - remnant.length) || 0);
-      if (i < 0)
-        i += all.length;
-      return i;
+    let select = function (candidate, all, count, flag) {
+      let carryover = function (all, remnant, count) {
+        let i = ((count && (count % all.length) - remnant.length) || 0);
+        return (((i < 0) && i + all.length) || i);
+      };
+      tabs.select(
+        (
+          candidate[parseInt((flag && (ts.getParentTab(gBrowser.mCurrentTab) && "0")) || (count || tap()))] || 
+          all[carryover(all, candidate, count)]
+        )._tPos
+      );
     };
+
     [
       [
         ['gj', 'sj'], 'Go to the next tab *loaded*.',
-        function (count) {
-          let right = bo.loaded(bo.right()), all = bo.loaded();
-          tabs.select(
-            (
-              right[(count || tap())] ||
-              all[carryover(all, right, count)]
-            )._tPos
-          );
-        }
+        function (count) select(bo.loaded(bo.right()), bo.loaded(), count),
       ], [
         ['gk', 'sk'], 'Go to the previous tab *loaded*.',
-        function (count) {
-          let left = bo.loaded(bo.left()).reverse(), all = bo.loaded().reverse();
-          tabs.select(
-            (
-              left[(count || tap())] ||
-              all[carryover(all, left, count)]
-            )._tPos
-          );
-        }
+        function (count) select(bo.loaded(bo.left()).reverse(), bo.loaded().reverse(), count),
       ], [
         ['gJ', 'sJ'], 'Go to the next *root* tab *loaded*.',
-        function (count) {
-          let parent = ts.getParentTab(gBrowser.mCurrentTab);
-          let right = bo.right(bo.loaded(ts.rootTabs)), all = bo.loaded(ts.rootTabs);
-          tabs.select(
-            (
-              right[(parent && 0) || (count || tap())] ||
-              all[carryover(all, right, count)]
-            )._tPos
-          );
-        }
+        function (count) select(bo.right(bo.loaded(ts.rootTabs)), bo.loaded(ts.rootTabs), count, true),
       ], [
         ['gK', 'sK'], 'Go to the previous *root* tab *loaded*.',
-        function (count) {
-          let parent = ts.getParentTab(gBrowser.mCurrentTab);
-          let left = bo.left(bo.loaded(ts.rootTabs)).reverse(), all = bo.loaded(ts.rootTabs).reverse();
-          tabs.select(
-            (
-              left[(parent && 0) || (count || tap())] ||
-              all[carryover(all, left, count)]
-            )._tPos
-          );
-        }
+        function (count) select(bo.left(bo.loaded(ts.rootTabs)).reverse(), bo.loaded(ts.rootTabs).reverse(), count, true),
       ],
     ].forEach(function ([keys, description, action]) mappings.addUserMap([modes.NORMAL], keys, description, action, {count: true}));
   };
