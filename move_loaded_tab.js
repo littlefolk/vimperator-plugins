986c73380a450958284dc1e888b6dd429fb5f4d1
littlefolk <littlefolk@gmail.com>
2010-06-22 11:37:18 +0900

    Changed, move_loaded_tab.js. Count is tabs carryover.
    カウントが候補のタブの数よりも多い場合、繰り越して計算した結果のタブを選択する。

--- move_loaded_tab.js
+++ move_loaded_tab.js
@@ -4,26 +4,32 @@
     let bo = liberator.plugins.browser_object_api;
     let ts = TreeStyleTabService;
     let tap = function () (bo.istap(gBrowser.mCurrentTab)? 0: 1);
+    let carryover = function (all, remnant, count) {
+      let i = ((count && (count % all.length) - remnant.length) || 0);
+      if (i < 0)
+        i += all.length;
+      return i;
+    };
     [
       [
         ['gj', 'sj'], 'Go to the next tab *loaded*.',
         function (count) {
+          let right = bo.loaded(bo.right()), all = bo.loaded();
           tabs.select(
             (
-              bo.loaded(bo.right())[(count || tap())] ||
-              bo.loaded()[((count && count - 1) || 0)] ||
-              bo.loaded().pop()
+              right[(count || tap())] ||
+              all[carryover(all, right, count)]
             )._tPos
           );
         }
       ], [
         ['gk', 'sk'], 'Go to the previous tab *loaded*.',
         function (count) {
+          let left = bo.loaded(bo.left()).reverse(), all = bo.loaded().reverse();
           tabs.select(
             (
-              bo.loaded(bo.left()).reverse()[(count || tap())] ||
-              bo.loaded().reverse()[((count && count - 1) || 0)] ||
-              bo.loaded().shift()
+              left[(count || tap())] ||
+              all[carryover(all, left, count)]
             )._tPos
           );
         }
@@ -31,11 +37,11 @@
         ['gJ', 'sJ'], 'Go to the next *root* tab *loaded*.',
         function (count) {
           let parent = ts.getParentTab(gBrowser.mCurrentTab);
+          let right = bo.right(bo.loaded(ts.rootTabs)), all = bo.loaded(ts.rootTabs);
           tabs.select(
             (
-              bo.right(bo.loaded(ts.rootTabs))[((parent)? 0: (count || tap()))] ||
-              bo.loaded(ts.rootTabs)[((count && count - 1) || 0)] ||
-              bo.loaded(ts.rootTabs).pop()
+              right[(parent && 0) || (count || tap())] ||
+              all[carryover(all, right, count)]
             )._tPos
           );
         }
@@ -43,11 +49,11 @@
         ['gK', 'sK'], 'Go to the previous *root* tab *loaded*.',
         function (count) {
           let parent = ts.getParentTab(gBrowser.mCurrentTab);
+          let left = bo.left(bo.loaded(ts.rootTabs)).reverse(), all = bo.loaded(ts.rootTabs).reverse();
           tabs.select(
             (
-              bo.left(bo.loaded(ts.rootTabs)).reverse()[(parent)? 0: (count || tap())] ||
-              bo.loaded(ts.rootTabs).reverse()[((count && count - 1) || 0)] ||
-              bo.loaded().shift()
+              left[(parent && 0) || (count || tap())] ||
+              all[carryover(all, left, count)]
             )._tPos
           );
         }
